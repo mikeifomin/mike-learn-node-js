cc72970860970354a16aed1a45c6f8f4a1eb4506
mike <mikeifomin@gmail.com>
2013-08-02 11:49:38 +0400

    step3

--- user.js
+++ user.js
@@ -1,15 +1,15 @@
 var private = "W";
+var phrase = require("./ru")
 
 function User(name){
     this.name = name;
 }
 
 User.prototype.hello = function(who){
-    console.log("hi, "+ who.name) ;
-
+    console.log(phrase.hello+ who.name);
 }
 
 
-console.log("user,js is loaded")
+console.log("user,js is loaded");
 
 exports.User = User
